e1fad4275d772e9fa51a454ae21a9aa2884833aa
Dwi Ratnasari <dwiratnasari391@gmail.com>
2017-05-29 14:00:33 +0700

    Add files via upload

---
+++ string.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Contoh Pemakaian Variabel</title>
+</head>
+<body>
+<?php
+$string='Belajar php MUDAH';
+$string2="Dan sangat menyenangkan";
+
+echo"$sring <br>" ;
+echo"string2" ; ?>
+</body>
+</html>
