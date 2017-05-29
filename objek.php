2eda8894320fbec1472577949a64d8459bd38ab5
Dwi Ratnasari <dwiratnasari391@gmail.com>
2017-05-29 13:57:15 +0700

    Add files via upload

---
+++ objek.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<tittl>Tutorial PHP</
+</
+<body>
+<?php
+class cara_makan
+{ function pisang()
+	{
+	return "dibuka kulitnya terlebih dahulu ";
+	}
+}
+$obj = new cara_makan;
+echo "cara makan pisang adalah dengan " . $obj->pisang();
+?>
+</body>
+</html>
