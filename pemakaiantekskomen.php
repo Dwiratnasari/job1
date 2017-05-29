7fcfa47eb905ab07f2f14b0d4a61a8576ff430d3
Dwi Ratnasari <dwiratnasari391@gmail.com>
2017-05-29 13:58:44 +0700

    Add files via upload

---
+++ pemakaiantekskomen.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Contoh pemakaian teks komentar</title>
+</head>
+<body>
+<?php
+/* echo "Aku sekarang bisa PHP! <br>") */ ?>
+
+<script language="php">
+echo ("tulis pake ini jika html editor kita tidak mengenali PHP");
+/* teks komentar satu baris*/
+
+</script>
+</body>
+</html>
